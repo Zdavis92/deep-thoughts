0ebba58f10b82641080e67c5935098c553dc2338
Zach Davis <zacharydavis92@gmail.com>
2022-05-14 10:47:12 -0500

    'add reactions form'

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,8 +1,40 @@
 import React, { useState } from "react";
+import { useMutation } from "@apollo/client";
+import { ADD_THOUGHT } from "../../utils/mutations";
+import { QUERY_THOUGHTS, QUERY_ME } from "../../utils/queries";
 
 const ThoughtForm = () => {
     const [thoughtText, setThoughtText] = useState('');
     const [characterCount, setCharacterCount] = useState(0);
+    const [addThought, { error }] = useMutation(ADD_THOUGHT, {
+        // this update is done so that new thoughts can be renderd without a new request to the server.
+        update(cache, { data: { addThought } }) { // addThought represents the new thought that was just created
+            // could potentially not exist yet, so wrap in a try/catch
+            try {
+                // update me array's cache
+                const { me } = cache.readQuery({ query: QUERY_ME });
+                cache.writeQuery({
+                    query: QUERY_ME,
+                    data: { me: { ...me, thoughts: [...me.thoughts, addThought] } },
+                });
+            } catch (e) {
+                console.warn('First thought insertion by user!')
+            }
+
+            try {
+                // read what's currently in the QUERY_THOUGHTS cache
+                const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+    
+                // prepend the newest thought to the front of the array
+                cache.writeQuery({
+                    query: QUERY_THOUGHTS,
+                    data: { thoughts: [addThought, ...thoughts] }
+                });
+            } catch (e) {
+                console.log(e);
+            }
+        }
+    });
 
     const handleChange = event => {
         if (event.target.value.length <= 280) {
@@ -13,14 +45,26 @@ const ThoughtForm = () => {
 
     const handleFormSubmit = async event => {
         event.preventDefault();
-        setThoughtText('');
-        setCharacterCount(0);
+
+        try {
+            // add thougth to database
+            await addThought({
+                variables: { thoughtText }
+            });
+
+            // clear form value
+            setThoughtText('');
+            setCharacterCount(0);
+        } catch (e) {
+            console.log(e);
+        }
     };
 
     return (
         <div>
-            <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
+            <p className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}>
                 Character Count: {characterCount}/280
+                {error && <span className="ml-2">Something went wrong...</span>}
             </p>
             <form
                 className="flex-row justify-center justify-space-between-md align-stretch"
